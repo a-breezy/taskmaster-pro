3b42965934d6ebc9d826ebb5d7ef7c4ee8b32368
a.breezy <ambrose.wilkins@gmail.com>
2021-11-02 14:00:27 -0400

    final styling with CSS on buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,15 +55,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function (event, ui) {
     console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event, ui) {
     console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
     console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
     console.log(event);
+    $(event.target).removeClass("dropover-active");
   },
   update: function () {
     var tempArr = [];
@@ -98,12 +104,15 @@ $("#trash").droppable({
   drop: function (event, ui) {
     // remove dragged element from the dom
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function (event, ui) {
     console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
     console.log(ui);
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
 });
 
@@ -120,7 +129,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -220,6 +229,7 @@ var auditTask = function (taskEl) {
   } else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
 // value of due date was changed
@@ -243,6 +253,13 @@ $(".list-group").on("change", "input[type='text']", function () {
   // pass tasks <li> element into auditTask() to check new due date
   auditTask($(taskSpan).closest(".list-group-item"));
 });
+
+// add timed refresh to the page
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 // remove all tasks
 $("#remove-tasks").on("click", function () {
